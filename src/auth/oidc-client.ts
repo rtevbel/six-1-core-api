97223850dcbb125721c1bff11283a1cc6b4a1188
alishoaib32 <shoaibcomsian@outlook.com>
2026-04-10 20:03:35 +0200

    refactor(auth): load Google OIDC credentials from environment

--- src/auth/oidc-client.ts
+++ src/auth/oidc-client.ts
@@ -1,20 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Issuer, Client } from 'openid-client';
+import { ensureDefinedConfigParam } from '../common/functions';
 
+/**
+ * OpenID Connect client for Google; credentials must be supplied via ConfigService / env.
+ */
+@Injectable()
 export class OidcClient {
-  async getClient() {
+  constructor(private readonly configService: ConfigService) {}
+
+  async getClient(): Promise<Client> {
     const googleIssuer = await Issuer.discover('https://accounts.google.com');
-    const client = new googleIssuer.Client({
+    const clientId = ensureDefinedConfigParam(
+      this.configService.get<string>('GOOGLE_CLIENT_ID'),
+      'GOOGLE_CLIENT_ID',
+    );
+    const clientSecret = ensureDefinedConfigParam(
+      this.configService.get<string>('GOOGLE_CLIENT_SECRET'),
+      'GOOGLE_CLIENT_SECRET',
+    );
+    const callbackUrl = ensureDefinedConfigParam(
+      this.configService.get<string>('GOOGLE_CALLBACK_URL'),
+      'GOOGLE_CALLBACK_URL',
+    );
+
+    return new googleIssuer.Client({
       authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
       tokenURL: 'https://oauth2.googleapis.com/token',
       userInfoURL: 'https://openidconnect.googleapis.com/v1/userinfo',
-      client_id:
-        'REDACTED_SET_GOOGLE_CLIENT_ID_VIA_ENV',
-      client_secret: 'REDACTED_SET_GOOGLE_CLIENT_SECRET_VIA_ENV',
-      redirect_uris: ['http://localhost:3005/auth/callback'],
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uris: [callbackUrl],
       scope: ['openid', 'profile', 'email'],
       responseType: 'code',
     });
-
-    return client;
   }
 }
